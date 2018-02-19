112961a3f69b48a7b4c58ae04b5bb2f41adc96aa
Shady Khalifa <shekohex@gmail.com>
2018-02-19 22:10:57 +0200

    feat(@nestjs/swagger) add the ability to work with Nest RouterModule.

--- lib/swagger-explorer.ts
+++ lib/swagger-explorer.ts
@@ -1,116 +1,161 @@
-import { METHOD_METADATA, PATH_METADATA } from '@nestjs/common/constants';
+import { METHOD_METADATA, PATH_METADATA } from "@nestjs/common/constants";
 import {
   exploreApiBearerMetadata,
-  exploreGlobalApiBearerMetadata,
-} from './explorers/api-bearer.explorer';
-import { exploreApiConsumesMetadata, exploreGlobalApiConsumesMetadata } from './explorers/api-consumes.explorer';
-import { exploreApiProducesMetadata, exploreGlobalApiProducesMetadata } from './explorers/api-produces.explorer';
-import { exploreApiResponseMetadata, exploreGlobalApiResponseMetadata } from './explorers/api-response.explorer';
-import { exploreApiUseTagsMetadata, exploreGlobalApiUseTagsMetadata } from './explorers/api-use-tags.explorer';
-import { isArray, isEmpty, mapValues, omitBy } from 'lodash';
-import { isUndefined, validatePath } from '@nestjs/common/utils/shared.utils';
+  exploreGlobalApiBearerMetadata
+} from "./explorers/api-bearer.explorer";
+import {
+  exploreApiConsumesMetadata,
+  exploreGlobalApiConsumesMetadata
+} from "./explorers/api-consumes.explorer";
+import {
+  exploreApiProducesMetadata,
+  exploreGlobalApiProducesMetadata
+} from "./explorers/api-produces.explorer";
+import {
+  exploreApiResponseMetadata,
+  exploreGlobalApiResponseMetadata
+} from "./explorers/api-response.explorer";
+import {
+  exploreApiUseTagsMetadata,
+  exploreGlobalApiUseTagsMetadata
+} from "./explorers/api-use-tags.explorer";
+import { isArray, isEmpty, mapValues, omitBy } from "lodash";
+import { isUndefined, validatePath } from "@nestjs/common/utils/shared.utils";
 
-import { Controller } from '@nestjs/common/interfaces';
-import { InstanceWrapper } from '@nestjs/core/injector/container';
-import { MetadataScanner } from '@nestjs/core/metadata-scanner';
-import { RequestMethod } from '@nestjs/common';
-import { exploreApiOperationMetadata } from './explorers/api-operation.explorer';
-import { exploreApiParametersMetadata } from './explorers/api-parameters.explorer';
+import { Controller } from "@nestjs/common/interfaces";
+import { InstanceWrapper } from "@nestjs/core/injector/container";
+import { MetadataScanner } from "@nestjs/core/metadata-scanner";
+import { RequestMethod } from "@nestjs/common";
+import { exploreApiOperationMetadata } from "./explorers/api-operation.explorer";
+import { exploreApiParametersMetadata } from "./explorers/api-parameters.explorer";
 
 export class SwaggerExplorer {
-    private readonly metadataScanner = new MetadataScanner();
-    private readonly modelsDefinitions = [];
+  private readonly metadataScanner = new MetadataScanner();
+  private readonly modelsDefinitions = [];
 
-    public exploreController({ instance, metatype }: InstanceWrapper<Controller>) {
-        const prototype = Object.getPrototypeOf(instance);
-        const explorersSchema = {
-            root: [
-                this.exploreRoutePathAndMethod,
-                exploreApiOperationMetadata,
-                exploreApiParametersMetadata.bind(null, this.modelsDefinitions),
-            ],
-            produces: [exploreApiProducesMetadata],
-            consumes: [exploreApiConsumesMetadata],
-            security: [exploreApiBearerMetadata],
-            tags: [exploreApiUseTagsMetadata],
-            responses: [exploreApiResponseMetadata.bind(null, this.modelsDefinitions)],
-        };
-        return this.generateDenormalizedDocument(metatype, prototype, instance, explorersSchema);
-    }
+  public exploreController(
+    { instance, metatype }: InstanceWrapper<Controller>,
+    modulePath: string
+  ) {
+    const prototype = Object.getPrototypeOf(instance);
+    const explorersSchema = {
+      root: [
+        this.exploreRoutePathAndMethod,
+        exploreApiOperationMetadata,
+        exploreApiParametersMetadata.bind(null, this.modelsDefinitions)
+      ],
+      produces: [exploreApiProducesMetadata],
+      consumes: [exploreApiConsumesMetadata],
+      security: [exploreApiBearerMetadata],
+      tags: [exploreApiUseTagsMetadata],
+      responses: [exploreApiResponseMetadata.bind(null, this.modelsDefinitions)]
+    };
+    return this.generateDenormalizedDocument(
+      metatype,
+      prototype,
+      instance,
+      explorersSchema,
+      modulePath
+    );
+  }
 
-    public getModelsDefinitons() {
-        return this.modelsDefinitions;
-    }
+  public getModelsDefinitons() {
+    return this.modelsDefinitions;
+  }
 
-    private generateDenormalizedDocument(metatype, prototype, instance, explorersSchema) {
-        const path = this.validateRoutePath(this.reflectControllerPath(metatype));
-
-        const self = this;
-        const globalMetadata = this.exploreGlobalMetadata(metatype);
-        const denormalizedPaths = this.metadataScanner.scanFromPrototype(instance, prototype, (name) => {
-            const targetCallback = prototype[name];
-            const methodMetadata = mapValues(explorersSchema, (explorers: any[]) => explorers.reduce((metadata, fn) => {
-                const exploredMetadata = fn.call(self, instance, prototype, targetCallback, path);
-                if (!exploredMetadata) {
-                  return metadata;
-                }
-                if (!isArray(exploredMetadata)) {
-                  return { ...metadata, ...exploredMetadata };
-                }
-                return isArray(metadata) ? [...metadata, ...exploredMetadata] : exploredMetadata;
-            }, {}));
-            return {
-                responses: {},
-                ...globalMetadata,
-                ...omitBy(methodMetadata, isEmpty),
-            };
-        });
-        return denormalizedPaths;
-    }
-
-    private exploreGlobalMetadata(metatype) {
-        const globalExplorers = [
-            exploreGlobalApiProducesMetadata,
-            exploreGlobalApiUseTagsMetadata,
-            exploreGlobalApiConsumesMetadata,
-            exploreGlobalApiBearerMetadata,
-            exploreGlobalApiResponseMetadata.bind(null, this.modelsDefinitions),
-        ];
-        const globalMetadata = (
-            globalExplorers.map((explorer) => explorer.call(explorer, metatype)).filter((val) => !isUndefined(val))
-        ).reduce((curr, next) => ({
-            ...curr,
-            ...next,
-        }), {});
-
-        return globalMetadata;
-    }
-
-    private exploreRoutePathAndMethod(instance, prototype, method, globalPath) {
-        const routePath = Reflect.getMetadata(PATH_METADATA, method);
-        if (isUndefined(routePath)) {
-            return undefined;
-        }
-        const requestMethod = Reflect.getMetadata(METHOD_METADATA, method) as RequestMethod;
-        const fullPath = globalPath + this.validateRoutePath(routePath);
+  private generateDenormalizedDocument(
+    metatype,
+    prototype,
+    instance,
+    explorersSchema,
+    modulePath
+  ) {
+    let path = this.validateRoutePath(this.reflectControllerPath(metatype));
+    if (modulePath)
+      path =
+        modulePath +
+        this.validateRoutePath(this.reflectControllerPath(metatype));
+    const self = this;
+    const globalMetadata = this.exploreGlobalMetadata(metatype);
+    const denormalizedPaths = this.metadataScanner.scanFromPrototype(
+      instance,
+      prototype,
+      name => {
+        const targetCallback = prototype[name];
+        const methodMetadata = mapValues(explorersSchema, (explorers: any[]) =>
+          explorers.reduce((metadata, fn) => {
+            const exploredMetadata = fn.call(
+              self,
+              instance,
+              prototype,
+              targetCallback,
+              path
+            );
+            if (!exploredMetadata) {
+              return metadata;
+            }
+            if (!isArray(exploredMetadata)) {
+              return { ...metadata, ...exploredMetadata };
+            }
+            return isArray(metadata)
+              ? [...metadata, ...exploredMetadata]
+              : exploredMetadata;
+          }, {})
+        );
         return {
-            method: RequestMethod[requestMethod].toLowerCase(),
-            path: fullPath === '' ? '/' : fullPath,
+          responses: {},
+          ...globalMetadata,
+          ...omitBy(methodMetadata, isEmpty)
         };
-    }
+      }
+    );
+    return denormalizedPaths;
+  }
 
-    private reflectControllerPath(metatype): string {
-        return Reflect.getMetadata(PATH_METADATA, metatype);
-    }
+  private exploreGlobalMetadata(metatype) {
+    const globalExplorers = [
+      exploreGlobalApiProducesMetadata,
+      exploreGlobalApiUseTagsMetadata,
+      exploreGlobalApiConsumesMetadata,
+      exploreGlobalApiBearerMetadata,
+      exploreGlobalApiResponseMetadata.bind(null, this.modelsDefinitions)
+    ];
+    const globalMetadata = globalExplorers
+      .map(explorer => explorer.call(explorer, metatype))
+      .filter(val => !isUndefined(val))
+      .reduce((curr, next) => ({ ...curr, ...next }), {});
 
-    private validateRoutePath(path: string): string {
-        if (isUndefined(path)) {
-            return '';
-        }
-        const pathWithParams = path.replace(/([:].*?[^\/]*)/g, (str) => {
-            str = str.replace(/\(.*\)$/, '');   // remove any regex in the param
-            return `{${str.slice(1, str.length)}}`;
-        });
-        return pathWithParams === '/' ? '' : validatePath(pathWithParams);
+    return globalMetadata;
+  }
+
+  private exploreRoutePathAndMethod(instance, prototype, method, globalPath) {
+    const routePath = Reflect.getMetadata(PATH_METADATA, method);
+    if (isUndefined(routePath)) {
+      return undefined;
     }
+    const requestMethod = Reflect.getMetadata(
+      METHOD_METADATA,
+      method
+    ) as RequestMethod;
+    const fullPath = globalPath + this.validateRoutePath(routePath);
+    return {
+      method: RequestMethod[requestMethod].toLowerCase(),
+      path: fullPath === "" ? "/" : fullPath
+    };
+  }
+
+  private reflectControllerPath(metatype): string {
+    return Reflect.getMetadata(PATH_METADATA, metatype);
+  }
+
+  private validateRoutePath(path: string): string {
+    if (isUndefined(path)) {
+      return "";
+    }
+    const pathWithParams = path.replace(/([:].*?[^\/]*)/g, str => {
+      str = str.replace(/\(.*\)$/, ""); // remove any regex in the param
+      return `{${str.slice(1, str.length)}}`;
+    });
+    return pathWithParams === "/" ? "" : validatePath(pathWithParams);
+  }
 }
--- lib/swagger-scanner.ts
+++ lib/swagger-scanner.ts
@@ -1,25 +1,44 @@
-import { SwaggerDocument } from './interfaces';
-import { SwaggerExplorer } from './swagger-explorer';
-import { flatten, groupBy, keyBy, mapValues, omit, reduce, extend } from 'lodash';
-import { SwaggerTransformer } from './swagger-transformer';
+import { MODULE_PATH } from "@nestjs/common/constants";
+
+import { SwaggerDocument } from "./interfaces";
+import { SwaggerExplorer } from "./swagger-explorer";
+import {
+  flatten,
+  groupBy,
+  keyBy,
+  mapValues,
+  omit,
+  reduce,
+  extend
+} from "lodash";
+import { SwaggerTransformer } from "./swagger-transformer";
 
 export class SwaggerScanner {
-    private readonly explorer = new SwaggerExplorer();
-    private readonly transfomer = new SwaggerTransformer();
+  private readonly explorer = new SwaggerExplorer();
+  private readonly transfomer = new SwaggerTransformer();
 
-    public scanApplication(app): SwaggerDocument {
-        const { container } = app;
-        const modules = container.getModules();
+  public scanApplication(app): SwaggerDocument {
+    const { container } = app;
+    const modules = container.getModules();
 
-        const denormalizedPaths = [...modules.values()].map(({ routes }) => this.scanModuleRoutes(routes));
-        return {
-            ...this.transfomer.normalizePaths(flatten(denormalizedPaths)),
-            definitions: reduce(this.explorer.getModelsDefinitons(), extend),
-        };
-    }
+    const denormalizedPaths = [...modules.values()].map(
+      ({ routes, metatype }) => {
+        const path = metatype
+          ? Reflect.getMetadata(MODULE_PATH, metatype)
+          : undefined;
+        return this.scanModuleRoutes(routes, path);
+      }
+    );
+    return {
+      ...this.transfomer.normalizePaths(flatten(denormalizedPaths)),
+      definitions: reduce(this.explorer.getModelsDefinitons(), extend)
+    };
+  }
 
-    public scanModuleRoutes(routes): SwaggerDocument {
-        const denormalizedArray = [...routes.values()].map((ctrl) => this.explorer.exploreController(ctrl));
-        return flatten(denormalizedArray) as any;
-    }
+  public scanModuleRoutes(routes, modulePath): SwaggerDocument {
+    const denormalizedArray = [...routes.values()].map(ctrl =>
+      this.explorer.exploreController(ctrl, modulePath)
+    );
+    return flatten(denormalizedArray) as any;
+  }
 }
