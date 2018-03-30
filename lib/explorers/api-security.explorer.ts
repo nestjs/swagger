3acbe881825c8db1fb06b5ced8be2e653c8c6cad
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-03-30 14:10:33 +0200

    Update api-security.explorer.ts

--- lib/explorers/api-security.explorer.ts
+++ lib/explorers/api-security.explorer.ts
@@ -3,25 +3,21 @@ import { DECORATORS } from '../constants';
 export const exploreGlobalApiSecurityMetadata = (metatype) => {
     const bearer = Reflect.getMetadata(DECORATORS.API_BEARER, metatype);
     const oauth2 = Reflect.getMetadata(DECORATORS.API_OAUTH2, metatype);
+   
     const security = [];
-    if (bearer) {
-        security.push({bearer});
-    }
-    if (oauth2) {
-        security.push({oauth2});
-    }
+    bearer && security.push({ bearer });
+    oauth2 && security.push({ oauth2 });
+    
     return security.length > 0 ? { security } : undefined;
 };
 
 export const exploreApiSecurityMetadata = (instance, prototype, method) => {
     const bearer = Reflect.getMetadata(DECORATORS.API_BEARER, method);
     const oauth2 = Reflect.getMetadata(DECORATORS.API_OAUTH2, method);
+    
     const security = [];
-    if (bearer) {
-        security.push({bearer});
-    }
-    if (oauth2) {
-        security.push({oauth2});
-    }
+    bearer && security.push({ bearer });
+    oauth2 && security.push({ oauth2 });
+    
     return oauth2 ? security : undefined;
 };
