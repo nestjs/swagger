4cedbeb6834b0b60844974cdf2a4de7274097715
Loïc BOUSQUET <loicbousquet93@gmail.com>
2018-05-22 16:11:10 +0200

    file upload decorator

--- lib/decorators/index.ts
+++ lib/decorators/index.ts
@@ -10,3 +10,4 @@ export * from './api-implicit-query.decorator';
 export * from './api-model-property.decorator';
 export * from './api-use-tags.decorator';
 export * from './api-oauth2.decorator';
+export * from './api-implicit-file.decorator';
