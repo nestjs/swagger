151f876df5278fbfbbd0b0fb8c764c072d9ea288
Loïc BOUSQUET <loicbousquet93@gmail.com>
2018-05-24 16:17:28 +0200

    Added param in decorator

--- lib/decorators/api-disable-endpoint.decorator.ts
+++ lib/decorators/api-disable-endpoint.decorator.ts
@@ -3,5 +3,7 @@ import { createMethodDecorator } from './helpers';
 import { pickBy, isNil, negate, isUndefined } from 'lodash';
 
 export const ApiDisableEndpoint = (): MethodDecorator => {
-  return createMethodDecorator(DECORATORS.API_DISABLE_ENDPOINT, {});
+  return createMethodDecorator(DECORATORS.API_DISABLE_ENDPOINT, {
+    disable: true
+  });
 };
