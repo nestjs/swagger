def064e9db3f8246eb822647d4554c3e5092d72e
Loïc BOUSQUET <loicbousquet93@gmail.com>
2018-05-30 15:20:11 +0200

    Revert "updated from nestjs/swagger"
    This reverts commit 5524c20e99f702df0d70d6a077b89dd380e1f1b1.

--- lib/swagger-scanner.ts
+++ lib/swagger-scanner.ts
@@ -7,11 +7,9 @@ import {
   mapValues,
   omit,
   reduce,
-  extend
+  extend,
 } from 'lodash';
 import { SwaggerTransformer } from './swagger-transformer';
-import { Logger } from '@nestjs/common';
-import * as util from 'util';
 
 export class SwaggerScanner {
   private readonly explorer = new SwaggerExplorer();
@@ -22,21 +20,17 @@ export class SwaggerScanner {
     const modules = container.getModules();
 
     const denormalizedPaths = [...modules.values()].map(({ routes }) =>
-      this.scanModuleRoutes(routes)
+      this.scanModuleRoutes(routes),
     );
-
-    const resb = this.transfomer.normalizePaths(flatten(denormalizedPaths));
-    const res = {
+    return {
       ...this.transfomer.normalizePaths(flatten(denormalizedPaths)),
-      definitions: reduce(this.explorer.getModelsDefinitons(), extend)
+      definitions: reduce(this.explorer.getModelsDefinitons(), extend),
     };
-    console.log(util.inspect(res.paths, false, null));
-    return res;
   }
 
   public scanModuleRoutes(routes): SwaggerDocument {
     const denormalizedArray = [...routes.values()].map(ctrl =>
-      this.explorer.exploreController(ctrl)
+      this.explorer.exploreController(ctrl),
     );
     return flatten(denormalizedArray) as any;
   }
