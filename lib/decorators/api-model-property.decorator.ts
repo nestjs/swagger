1c6ed4efbcee86e464366b983ad50d2048ba8bed
Weeco <weeco91@gmail.com>
2018-08-22 23:09:47 +0200

    Fixes #125
    Add ApiResponseModelProperty

--- lib/decorators/api-model-property.decorator.ts
+++ lib/decorators/api-model-property.decorator.ts
@@ -66,3 +66,13 @@ export const ApiModelPropertyOptional = (
     ...metadata,
     required: false
   });
+
+export const ApiResponseModelProperty = (
+  metadata: {
+    type?: any;
+    example?: any;
+  } = {}
+): PropertyDecorator =>
+  ApiModelProperty({
+    ...metadata
+  });
