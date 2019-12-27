dda3df9324fcd96239e9ea02928e2805d0973ff9
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-12-27 12:54:41 +0100

    chore(): publish 4.1.2 release

--- lib/utils/get-schema-path.util.ts
+++ lib/utils/get-schema-path.util.ts
@@ -1,4 +1,7 @@
-export function getSchemaPath(modelName: string): string {
+import { isString } from 'util';
+
+export function getSchemaPath(model: string | Function): string {
+  const modelName = isString(model) ? model : model && model.name;
   return `#/components/schemas/${modelName}`;
 }
 
