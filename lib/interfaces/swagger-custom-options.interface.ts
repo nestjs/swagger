20c7f4918379c5c875850db3388cdf8bb2b16dc6
Elior Boukhobza <heliosaian@gmail.com>
2020-04-10 18:49:45 +0300

    Update swagger-custom-options.interface.ts
    chore(swagger-custom-options.interface.ts): Update the SwaggerCustomOptions interface according to https://github.com/scottie1984/swagger-ui-express

--- lib/interfaces/swagger-custom-options.interface.ts
+++ lib/interfaces/swagger-custom-options.interface.ts
@@ -2,8 +2,12 @@ export interface SwaggerCustomOptions {
   explorer?: boolean;
   swaggerOptions?: any;
   customCss?: string;
+  customCssUrl?: string;
   customJs?: string;
   customfavIcon?: string;
   swaggerUrl?: string;
   customSiteTitle?: string;
+  validatorUrl?: string;
+  url?: string;
+  urls?: {url: string, name: string}[];
 }
