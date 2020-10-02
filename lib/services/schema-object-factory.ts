cf98aaba9845b65222ccf55726c7e85fb8009437
Stefan Radacovsky <stefan.radacovsky@softec.cz>
2020-10-02 23:59:39 +0200

    feat(@nestjs/swagger): Allow custom schema naming
    You can use @ApiSchema to set a custom schema name.
    Custom schema names are not inherited.

--- lib/services/schema-object-factory.ts
+++ lib/services/schema-object-factory.ts
@@ -10,6 +10,7 @@ import {
   omitBy
 } from 'lodash';
 import { DECORATORS } from '../constants';
+import { ApiSchemaOptions } from '../decorators';
 import { getTypeIsArrayTuple } from '../decorators/helpers';
 import { exploreGlobalApiExtraModelsMetadata } from '../explorers/api-extra-models.explorer';
 import {
@@ -152,10 +153,19 @@ export class SchemaObjectFactory {
     if (typeDefinitionRequiredFields.length > 0) {
       typeDefinition['required'] = typeDefinitionRequiredFields;
     }
+    // Find out if user defined custom schema name. If not then use the type name.
+    const customSchema: ApiSchemaOptions[] = Reflect.getOwnMetadata(
+      DECORATORS.API_SCHEMA,
+      type
+    );
+    const schemaName =
+      customSchema && customSchema.length === 1
+        ? customSchema[0].name
+        : type.name;
     schemas.push({
-      [type.name]: typeDefinition
+      [schemaName]: typeDefinition
     });
-    return type.name;
+    return schemaName;
   }
 
   mergePropertyWithMetadata(
