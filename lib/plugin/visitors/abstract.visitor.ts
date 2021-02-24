67682bba81b340631b36d839b7137c624a0c42b9
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2021-02-24 12:55:07 +0100

    fix(plugin): support typescript 4.2+

--- lib/plugin/visitors/abstract.visitor.ts
+++ lib/plugin/visitors/abstract.visitor.ts
@@ -7,27 +7,54 @@ export class AbstractFileVisitor {
     factory: ts.NodeFactory | undefined
   ): ts.SourceFile {
     if (!factory) {
+      // support TS v4.2+
+      const importEqualsDeclaration =
+        ts.createImportEqualsDeclaration.length === 5
+          ? (ts.createImportEqualsDeclaration as any)(
+              undefined,
+              undefined,
+              false,
+              OPENAPI_NAMESPACE,
+              ts.createExternalModuleReference(
+                ts.createLiteral(OPENAPI_PACKAGE_NAME)
+              )
+            )
+          : (ts.createImportEqualsDeclaration as any)(
+              undefined,
+              undefined,
+              OPENAPI_NAMESPACE,
+              ts.createExternalModuleReference(
+                ts.createLiteral(OPENAPI_PACKAGE_NAME)
+              )
+            );
       return ts.updateSourceFileNode(sourceFile, [
-        ts.createImportEqualsDeclaration(
-          undefined,
-          undefined,
-          OPENAPI_NAMESPACE,
-          ts.createExternalModuleReference(
-            ts.createLiteral(OPENAPI_PACKAGE_NAME)
-          )
-        ),
+        importEqualsDeclaration,
         ...sourceFile.statements
       ]);
     }
+    // support TS v4.2+
+    const importEqualsDeclaration =
+      factory.createImportEqualsDeclaration.length === 5
+        ? (factory.createImportEqualsDeclaration as any)(
+            undefined,
+            undefined,
+            false,
+            OPENAPI_NAMESPACE,
+            factory.createExternalModuleReference(
+              factory.createStringLiteral(OPENAPI_PACKAGE_NAME)
+            )
+          )
+        : (factory.createImportEqualsDeclaration as any)(
+            undefined,
+            undefined,
+            OPENAPI_NAMESPACE,
+            factory.createExternalModuleReference(
+              factory.createStringLiteral(OPENAPI_PACKAGE_NAME)
+            )
+          );
+
     return factory.updateSourceFile(sourceFile, [
-      factory.createImportEqualsDeclaration(
-        undefined,
-        undefined,
-        OPENAPI_NAMESPACE,
-        factory.createExternalModuleReference(
-          factory.createStringLiteral(OPENAPI_PACKAGE_NAME)
-        )
-      ),
+      importEqualsDeclaration,
       ...sourceFile.statements
     ]);
   }
