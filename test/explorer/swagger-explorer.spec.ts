9d25dc97d328319dd510c94f864225e961c13d00
Artur Kraft <schokorussie@gmail.com>
2021-09-03 16:08:55 +0200

    fix merge error

--- test/explorer/swagger-explorer.spec.ts
+++ test/explorer/swagger-explorer.spec.ts
@@ -463,7 +463,6 @@ describe('SwaggerExplorer', () => {
           required: true,
           schema: {
             type: 'string',
-            format: 'uuid'
           }
         },
         {
@@ -640,6 +639,7 @@ describe('SwaggerExplorer', () => {
           name: 'objectId',
           required: true,
           schema: {
+            format: 'uuid',
             type: 'string'
           }
         }
