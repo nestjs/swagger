0e4f9e64620a9d0bfdd623e5022070216d70d20a
Shean de Montigny-Desautels <s.montigny-desautels@softdb.com>
2021-09-06 21:20:59 -0400

    fix(plugin): Handle nullable enum and prevent empty enum key

--- test/plugin/fixtures/nullable.dto.ts
+++ test/plugin/fixtures/nullable.dto.ts
@@ -1,9 +1,16 @@
 export const nullableDtoText = `
+enum Status {
+    ENABLED,
+    DISABLED
+}
+
 export class NullableDto {
   @ApiProperty()
   stringValue: string | null;
   @ApiProperty()
   stringArr: string[] | null;
+  @ApiProperty()
+  enumValue: Status | null;
   @ApiProperty()
   optionalString?: string;
   @ApiProperty()
@@ -11,9 +18,14 @@ export class NullableDto {
 }
 `;
 
-export const nullableDtoTextTranspiled = `export class NullableDto {
+export const nullableDtoTextTranspiled = `var Status;
+(function (Status) {
+    Status[Status["ENABLED"] = 0] = "ENABLED";
+    Status[Status["DISABLED"] = 1] = "DISABLED";
+})(Status || (Status = {}));
+export class NullableDto {
     static _OPENAPI_METADATA_FACTORY() {
-        return { stringValue: { required: true, type: () => String, nullable: true }, stringArr: { required: true, type: () => [String], nullable: true }, optionalString: { required: false, type: () => String }, undefinedString: { required: true, type: () => String } };
+        return { stringValue: { required: true, type: () => String, nullable: true }, stringArr: { required: true, type: () => [String], nullable: true }, enumValue: { required: true, nullable: true, enum: Status }, optionalString: { required: false, type: () => String }, undefinedString: { required: true, type: () => String } };
     }
 }
 __decorate([
@@ -22,6 +34,9 @@ __decorate([
 __decorate([
     ApiProperty()
 ], NullableDto.prototype, "stringArr", void 0);
+__decorate([
+    ApiProperty()
+], NullableDto.prototype, "enumValue", void 0);
 __decorate([
     ApiProperty()
 ], NullableDto.prototype, "optionalString", void 0);
