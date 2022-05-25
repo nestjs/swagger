7b683119e18ecd047d07e2224ed17be6696c1c30
Matthew Wagerfield <matthew@wagerfield.com>
2022-05-25 11:06:18 +0100

    chore: format code with prettier

--- test/type-helpers/intersection-type.helper.spec.ts
+++ test/type-helpers/intersection-type.helper.spec.ts
@@ -1,6 +1,12 @@
 import { Type } from '@nestjs/common';
 import { Expose, Transform } from 'class-transformer';
-import { IsDateString, IsEmail, IsString, MinLength, validateSync } from 'class-validator';
+import {
+  IsDateString,
+  IsEmail,
+  IsString,
+  MinLength,
+  validateSync
+} from 'class-validator';
 import { ApiProperty } from '../../lib/decorators';
 import { METADATA_FACTORY_NAME } from '../../lib/plugin/plugin-constants';
 import { ModelPropertiesAccessor } from '../../lib/services/model-properties-accessor';
@@ -73,26 +79,26 @@ describe('IntersectionType', () => {
     it('should validate property values', () => {
       const dto = new UpdateUserDto();
 
-      expect(validateSync(dto)).toHaveLength(4)
+      expect(validateSync(dto)).toHaveLength(4);
 
-      dto.firstName = "John"
-      dto.email = "john.doe@test.com"
-      dto.createdAt = "2022-05-25T10:46:00"
+      dto.firstName = 'John';
+      dto.email = 'john.doe@test.com';
+      dto.createdAt = '2022-05-25T10:46:00';
 
-      expect(validateSync(dto)).toHaveLength(1)
-      
-      dto.password = "Test123!"
+      expect(validateSync(dto)).toHaveLength(1);
 
-      expect(validateSync(dto)).toHaveLength(0)
-      
-      dto.email = "john.doe@test.x"
-      dto.password = "2short!"
+      dto.password = 'Test123!';
 
-      const errors = validateSync(dto)
+      expect(validateSync(dto)).toHaveLength(0);
 
-      expect(errors).toHaveLength(2)
-      expect(errors[0].constraints).toHaveProperty("isEmail")
-      expect(errors[1].constraints).toHaveProperty("minLength")
+      dto.email = 'john.doe@test.x';
+      dto.password = '2short!';
+
+      const errors = validateSync(dto);
+
+      expect(errors).toHaveLength(2);
+      expect(errors[0].constraints).toHaveProperty('isEmail');
+      expect(errors[1].constraints).toHaveProperty('minLength');
     });
   });
 });
