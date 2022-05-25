770a26ad6c45c79d0a61911132d53f61dc2763a3
Matthew Wagerfield <matthew@wagerfield.com>
2022-05-25 11:03:34 +0100

    feat: add support for multiple source classes

--- test/type-helpers/intersection-type.helper.spec.ts
+++ test/type-helpers/intersection-type.helper.spec.ts
@@ -1,6 +1,6 @@
 import { Type } from '@nestjs/common';
 import { Expose, Transform } from 'class-transformer';
-import { IsString } from 'class-validator';
+import { IsDateString, IsEmail, IsString, MinLength, validateSync } from 'class-validator';
 import { ApiProperty } from '../../lib/decorators';
 import { METADATA_FACTORY_NAME } from '../../lib/plugin/plugin-constants';
 import { ModelPropertiesAccessor } from '../../lib/services/model-properties-accessor';
@@ -8,13 +8,15 @@ import { IntersectionType } from '../../lib/type-helpers';
 
 describe('IntersectionType', () => {
   class CreateUserDto {
+    @IsEmail()
     @ApiProperty({ required: true })
-    login: string;
+    email: string;
 
     @Expose()
     @Transform((str) => str + '_transformed')
+    @MinLength(8)
     @IsString()
-    @ApiProperty({ minLength: 10 })
+    @ApiProperty({ minLength: 8 })
     password: string;
 
     static [METADATA_FACTORY_NAME]() {
@@ -26,13 +28,23 @@ describe('IntersectionType', () => {
     @IsString()
     @ApiProperty({ required: false })
     firstName: string;
+  }
+
+  class MetaDto {
+    @IsDateString()
+    @ApiProperty({ required: false })
+    createdAt: string;
 
     static [METADATA_FACTORY_NAME]() {
-      return { dateOfBirth2: { required: true, type: () => String } };
+      return { userId: { required: true, type: () => String } };
     }
   }
 
-  class UpdateUserDto extends IntersectionType(UserDto, CreateUserDto) {}
+  class UpdateUserDto extends IntersectionType(
+    MetaDto,
+    UserDto,
+    CreateUserDto
+  ) {}
 
   let modelPropertiesAccessor: ModelPropertiesAccessor;
 
@@ -40,18 +52,47 @@ describe('IntersectionType', () => {
     modelPropertiesAccessor = new ModelPropertiesAccessor();
   });
 
-  describe('OpenAPI metadata', () => {
+  describe('OpenAPI Metadata', () => {
     it('should return combined class', () => {
-      const prototype = (UpdateUserDto.prototype as any) as Type<unknown>;
+      const prototype = UpdateUserDto.prototype as any as Type<unknown>;
 
       modelPropertiesAccessor.applyMetadataFactory(prototype);
+
       expect(modelPropertiesAccessor.getModelProperties(prototype)).toEqual([
+        'createdAt',
         'firstName',
-        'login',
+        'email',
         'password',
-        'dateOfBirth2',
+        'userId',
         'dateOfBirth'
       ]);
+    });
+  });
+
+  describe('Class Validation', () => {
+    it('should validate property values', () => {
+      const dto = new UpdateUserDto();
+
+      expect(validateSync(dto)).toHaveLength(4)
+
+      dto.firstName = "John"
+      dto.email = "john.doe@test.com"
+      dto.createdAt = "2022-05-25T10:46:00"
+
+      expect(validateSync(dto)).toHaveLength(1)
+      
+      dto.password = "Test123!"
+
+      expect(validateSync(dto)).toHaveLength(0)
+      
+      dto.email = "john.doe@test.x"
+      dto.password = "2short!"
+
+      const errors = validateSync(dto)
+
+      expect(errors).toHaveLength(2)
+      expect(errors[0].constraints).toHaveProperty("isEmail")
+      expect(errors[1].constraints).toHaveProperty("minLength")
     });
   });
 });
