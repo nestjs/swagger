b517b7fc46bc7fcc8f5819ea51596ebdaa272b83
Tran Quang <randytran8800@gmail.com>
2023-08-28 17:17:00 +0700

    fix(services): resolve enum type correctly if enumName is provided

--- lib/services/schema-object-factory.ts
+++ lib/services/schema-object-factory.ts
@@ -294,7 +294,7 @@ export class SchemaObjectFactory {
 
     if (!(enumName in schemas)) {
       schemas[enumName] = {
-        type: 'string',
+        type: metadata.type as string,
         enum:
           metadata.isArray && metadata.items
             ? metadata.items['enum']
