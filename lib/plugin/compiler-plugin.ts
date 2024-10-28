a248d0bea2fa526b5f2ef839fce5c3615fa256d9
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2024-10-28 10:13:20 +0100

    fix: throw error when isolated modules true #2972

--- lib/plugin/compiler-plugin.ts
+++ lib/plugin/compiler-plugin.ts
@@ -1,5 +1,6 @@
 import * as ts from 'typescript';
 import { mergePluginOptions } from './merge-options';
+import { pluginDebugLogger } from './plugin-debug-logger';
 import { isFilenameMatched } from './utils/is-filename-matched.util';
 import { ControllerClassVisitor } from './visitors/controller-class.visitor';
 import { ModelClassVisitor } from './visitors/model-class.visitor';
@@ -9,6 +10,12 @@ const controllerClassVisitor = new ControllerClassVisitor();
 
 export const before = (options?: Record<string, any>, program?: ts.Program) => {
   options = mergePluginOptions(options);
+
+  if (!program) {
+    const error = `The "program" reference must be provided when using the CLI Plugin. This error is likely caused by the "isolatedModules" compiler option being set to "true".`;
+    pluginDebugLogger.debug(error);
+    throw new Error(error);
+  }
 
   return (ctx: ts.TransformationContext): ts.Transformer<any> => {
     return (sf: ts.SourceFile) => {
