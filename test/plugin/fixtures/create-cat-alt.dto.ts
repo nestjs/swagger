3e968ba26795138815fbc09f9f47d002e564b2f7
Radosław Kłos <radoslaw@klos.dev>
2025-02-27 18:31:50 +0100

    feat: support native private class properties in model class visitor

--- test/plugin/fixtures/create-cat-alt.dto.ts
+++ test/plugin/fixtures/create-cat-alt.dto.ts
@@ -42,10 +42,12 @@ export class CreateCatDto2 {
     [x: string]: string;
   }
   amount: bigint;
+  #privateProperty: string;
 }
 `;
 
-export const createCatDtoTextAltTranspiled = `import * as openapi from "@nestjs/swagger";
+export const createCatDtoTextAltTranspiled = `var _CreateCatDto2_privateProperty;
+import * as openapi from "@nestjs/swagger";
 import * as package from 'class-validator';
 var Status;
 (function (Status) {
@@ -56,11 +58,13 @@ export class CreateCatDto2 {
     constructor() {
         this.age = 3;
         this.status = Status.ENABLED;
+        _CreateCatDto2_privateProperty.set(this, void 0);
     }
     static _OPENAPI_METADATA_FACTORY() {
         return { name: { required: true, type: () => String }, age: { required: true, type: () => Number, default: 3 }, tags: { required: true, type: () => [String] }, status: { required: true, default: Status.ENABLED, enum: Status }, breed: { required: false, type: () => String }, nodes: { required: true, type: () => [Object] }, alias: { required: true, type: () => Object }, numberAlias: { required: true, type: () => Number, description: "NumberAlias" }, union: { required: true, type: () => Object }, intersection: { required: true, type: () => Object }, nested: { required: true, type: () => ({ first: { required: true, type: () => String }, second: { required: true, type: () => Number }, status: { required: true, enum: Status }, tags: { required: true, type: () => [String] }, nodes: { required: true, type: () => [Object] }, alias: { required: true, type: () => Object }, numberAlias: { required: true, type: () => Number } }) }, amount: { required: true, type: () => BigInt } };
     }
 }
+_CreateCatDto2_privateProperty = new WeakMap();
 __decorate([
     package.IsString()
 ], CreateCatDto2.prototype, "name", void 0);
--- test/plugin/fixtures/create-cat-alt2.dto.ts
+++ test/plugin/fixtures/create-cat-alt2.dto.ts
@@ -81,16 +81,31 @@ export abstract class Audit {
    * @example 5
    */
   testNumber: number;
+
+  /**
+   * privateProperty
+   * @example 'secret'
+   */
+  #privateProperty: string;
 }
 `;
 
-export const createCatDtoTextAlt2Transpiled = `import * as openapi from "@nestjs/swagger";
+export const createCatDtoTextAlt2Transpiled = `var _Audit_privateProperty;
+import * as openapi from "@nestjs/swagger";
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 export class Audit {
+    constructor() {
+        /**
+         * privateProperty
+         * @example 'secret'
+         */
+        _Audit_privateProperty.set(this, void 0);
+    }
     static _OPENAPI_METADATA_FACTORY() {
         return { createdAt: { required: true, type: () => Object, description: "test on createdAt" }, updatedAt: { required: true, type: () => Object }, version: { required: true, type: () => String, description: "test\\nversion", example: "version 123" }, testVersion: { required: true, type: () => Object, description: "testVersion", examples: ["0.0.1", "0.0.2"], deprecated: true }, testVersion2: { required: true, type: () => Object, description: "testVersion2", examples: ["0.0.1", "0.0.2"], deprecated: true }, testVersionArray: { required: true, type: () => [String], description: "testVersionArray", example: ["0.0.1", "0.0.2"] }, testVersionArray2: { required: true, type: () => [String], description: "testVersionArray", example: ["version 123", "version 321"] }, testVersionArray3: { required: true, type: () => [Number], description: "testVersionArray", example: [123, 321] }, testBoolean: { required: true, type: () => Boolean, description: "testBoolean", example: true }, testNumber: { required: true, type: () => Number, description: "testNumber", examples: [1, 5] } };
     }
 }
+_Audit_privateProperty = new WeakMap();
 __decorate([
     CreateDateColumn()
 ], Audit.prototype, "createdAt", void 0);
